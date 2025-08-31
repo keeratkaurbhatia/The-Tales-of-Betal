fe1b98e13732df3948963fbe35798f054abcfd6b
Keerat <itskeeratkaur@gmail.com>
2025-08-31 23:19:56 +0530

    minor tweaks

--- src/components/StoryPlayer.tsx
+++ src/components/StoryPlayer.tsx
@@ -133,7 +133,7 @@ const StoryPlayer = ({ story, onStoryEnd }: StoryPlayerProps) => {
           
           <div className="flex items-center space-x-2 text-purple-200">
             <Volume2 className="w-5 h-5" />
-            <span className="text-sm">Male Voice • Indian Accent</span>
+            <span className="text-sm">Indian Accent</span>
           </div>
         </div>
 
